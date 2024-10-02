099a8bc2243f397ff93b43a5073246065f4c293a
piseth sattya <pisethsattya33@gmail.com>
2024-10-02 22:21:10 +0700

    set up api gateway with resilience4j

--- src/main/java/guru/techinsights/microservice/api_gateway/config/SecurityConfig.java
+++ src/main/java/guru/techinsights/microservice/api_gateway/config/SecurityConfig.java
@@ -12,10 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @Configuration
 public class SecurityConfig {
+    private final String[] freeResourceUrls = {"/docs/**","/swagger-ui/**","/api-docs/**","/v3/api-docs/**","/aggregate/**"};
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-      return httpSecurity.authorizeRequests(authorize -> authorize.anyRequest()
-                        .authenticated())
+      return httpSecurity.authorizeHttpRequests(authorize ->
+                      authorize
+                              .requestMatchers(freeResourceUrls).permitAll()
+                              .anyRequest().authenticated())
                         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                         .build();
 
